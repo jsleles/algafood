44e5897353858cd001293b02d6c879b29a404bc1
Jonilson da Silva Leles <jsleles@gmail.com>
2025-08-07 20:55:04 -0300

    implementado até 4.22

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,9 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -19,13 +24,28 @@ public class CozinhaController {
 	CozinhaRepository cozinhaRepository;
 	
     @GetMapping
-	public List<Cozinha> lista() {
-        return cozinhaRepository.listar();	
+	public ResponseEntity<List<Cozinha>>   lista() {
+        
+    	List<Cozinha> cozinha = cozinhaRepository.listar();
+    	return ResponseEntity.ok(cozinha);	
 	}
     
     @GetMapping("/{cozinhaId}")
-    public Cozinha busca(@PathVariable  Long cozinhaId) {
-    	return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> busca(@PathVariable  Long cozinhaId) {
+    	Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+        
+    	if (cozinha != null) {
+    		return ResponseEntity.ok(cozinha);
+    	}
+    	
+//    	return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    	return ResponseEntity.notFound().build();
     }
 
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+    	return cozinhaRepository.salvar(cozinha);
+    }
 }
--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -3,9 +3,14 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -19,14 +24,25 @@ public class RestauranteController {
 	RestauranteRepository restauranteRepository;
 	
 	@GetMapping
-	public List<Restaurante> lista(){
-        return restauranteRepository.listar();		
+	public ResponseEntity<List<Restaurante>>  lista(){
+		List<Restaurante> restaurante = restauranteRepository.listar();	
+        return 	ResponseEntity.ok(restaurante);
 	}
 
 	@GetMapping("/{restauranteId}")
-	public Restaurante busca(@PathVariable  Long restauranteId) {
-		return restauranteRepository.buscar(restauranteId);
+	public ResponseEntity<Restaurante>  busca(@PathVariable  Long restauranteId) {
+		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
 		
+		if (restaurante != null) {
+			return ResponseEntity.ok(restaurante);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Restaurante adicionar(@RequestBody Restaurante restaurante) {
+		 return restauranteRepository.salvar(restaurante);
+	}
 }
