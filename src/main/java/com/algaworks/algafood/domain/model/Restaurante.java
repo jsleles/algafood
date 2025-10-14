b0e00d9158654679441a9c571b8625b39b7007b3
Jonilson da Silva Leles <jsleles@gmail.com>
2025-10-14 20:42:11 -0300

    Implementado até 6.5

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -4,7 +4,10 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +38,11 @@ public class Restaurante {
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
  	
+ 	@JsonIgnore
+ 	@Embedded
+ 	private Endereco endereco;
+ 	
+ 	@JsonIgnore
  	@ManyToMany
  	@JoinTable (name="restaurante_forma_pagamento",
  	     joinColumns = @JoinColumn(name="restaurante_id"),
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,22 @@
 insert into cozinha (id,nome) values (1,'Tailandesa');
 insert into cozinha (id,nome) values (2,'Indiana');
 
-insert into restaurante (nome, taxa_frete,cozinha_id) values ('O melhor da Tailandia', 5,1);
+insert into estado (nome) values ('SP');
+insert into estado (nome) values ('RJ');
+insert into estado (nome) values ('RS');
+insert into estado (nome) values ('MG');
+
+insert into cidade (estado_id, nome) values (1,'São Bernardo do Campo');
+insert into cidade (estado_id, nome) values (1,'São Paulo');
+insert into cidade (estado_id, nome) values (1,'São Caetano');
+insert into cidade (estado_id, nome) values (1,'Santo André');
+insert into cidade (estado_id, nome) values (2,'Rio de Janeiro');
+insert into cidade (estado_id, nome) values (2,'Copacabana');
+insert into cidade (estado_id, nome) values (2,'Niteroi');
+insert into cidade (estado_id, nome) values (3,'Porto Alegre');
+insert into cidade (estado_id, nome) values (3,'Charqueada');
+
+insert into restaurante (nome, taxa_frete,cozinha_id, endereco_cidade_id, endereco_bairro, endereco_cep,endereco_complemento, endereco_logradouro, endereco_numero) values ('O melhor da Tailandia', 5,1, 1,'Centro', '09750-225','apto 193C','Rua Dr Marcel Preotesco','134');
 insert into restaurante (nome, taxa_frete,cozinha_id) values ('Comida Indiana', 8,2);
 insert into restaurante (nome, taxa_frete,cozinha_id) values ('Comida Indiana xpto', 3,2);
 
@@ -17,22 +32,6 @@ insert into permissao (nome, descricao) values ('NOR','Usuário normal');
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1),(1,2),(1,3),(2,3),(3,2),(3,3);
 
-insert into estado (nome) values ('SP');
-insert into estado (nome) values ('RJ');
-insert into estado (nome) values ('RS');
-insert into estado (nome) values ('MG');
-
-insert into cidade (estado_id, nome) values (1,'São Bernardo do Campo');
-insert into cidade (estado_id, nome) values (1,'São Paulo');
-insert into cidade (estado_id, nome) values (1,'São Caetano');
-insert into cidade (estado_id, nome) values (1,'Santo André');
-
-insert into cidade (estado_id, nome) values (2,'Rio de Janeiro');
-insert into cidade (estado_id, nome) values (2,'Copacabana');
-insert into cidade (estado_id, nome) values (2,'Niteroi');
-
-insert into cidade (estado_id, nome) values (3,'Porto Alegre');
-insert into cidade (estado_id, nome) values (3,'Charqueada');
 
 
 
